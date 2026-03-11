5a6e19afce665afd90c464284f6f5bfbf5bf483d
Mital Andipara <mital@logisticinfotech.co.in>
2026-03-11 15:07:33 +0530

    changes

--- api/controllers/api/v1/user/follow/followed-list.js
+++ api/controllers/api/v1/user/follow/followed-list.js
@@ -1,34 +1,18 @@
 module.exports = {
     friendlyName: "Follower list",
-    inputs: {
-        userId: {
-            type: "number",
-            required: true,
-        },
-        followType: {
-            type: "number",  // 1 then followList 2 then followingList
-            required: true,
-        },
-    },
+    inputs: {},
     exits: {
         success: sails.config.globals.statusCodes.success,
         invalid: sails.config.globals.statusCodes.invalid,
     },
     fn: async function (inputs, exits) {
         const loginUserData = this.req.loggedInUser;
-        const { userId, followType } = inputs;
-
-        const blockedUserIds = await sails.helpers.getBlockUnactiveUserlist(loginUserData?.id);        
-
-        const filter = (followType === 1)
-            ? { followedTo: userId, followedBy: { nin: blockedUserIds } }
-            : { followedBy: userId, followedTo: { nin: blockedUserIds } };
 
         const data = await sails.helpers.getPaginationList.with({
             request: this.req,
             modelName: "Follower",
-            whereCondition: filter,
-            populate: followType === 1 ? ["followedTo"] : ["followedBy"],
+            whereCondition: { followedBy: loginUserData?.id },
+            populate: ["followedTo"],
         });
 
         if (data.status) {
@@ -38,28 +22,3 @@ module.exports = {
         }
     },
 };
-
-// module.exports = {
-//     friendlyName: "Follower list",
-//     inputs: {},
-//     exits: {
-//         success: sails.config.globals.statusCodes.success,
-//         invalid: sails.config.globals.statusCodes.invalid,
-//     },
-//     fn: async function (inputs, exits) {
-//         const loginUserData = this.req.loggedInUser;
-
-//         const data = await sails.helpers.getPaginationList.with({
-//             request: this.req,
-//             modelName: "Follower",
-//             whereCondition: { followedBy: loginUserData?.id },
-//             populate: ["followedTo"],
-//         });
-
-//         if (data.status) {
-//             return exits.success(data);
-//         } else {
-//             return exits.invalid(data);
-//         }
-//     },
-// };
